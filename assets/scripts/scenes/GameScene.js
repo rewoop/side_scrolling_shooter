19074bd10f2131414e77c77541fe8727655819de
rewoop <10rubleqow@gmail.com>
2021-05-10 16:56:07 +0300

    Adds boom animation.

--- assets/scripts/scenes/GameScene.js
+++ assets/scripts/scenes/GameScene.js
@@ -25,9 +25,11 @@ class GameScene extends Phaser.Scene {
 	}
 
 	onOverlap(source, target) {
-		if (source !== this.player && target !== this.player) {
+		const enemy = [source, target].find(item => item.texture.key === 'enemy');
+		if (enemy) {
 			this.score++;
 			this.scoreText.setText(`Score: ${this.score}`);
+			Boom.generate(this, enemy.x, enemy.y);
 		}
 		source.setAlive(false);
 		target.setAlive(false);
