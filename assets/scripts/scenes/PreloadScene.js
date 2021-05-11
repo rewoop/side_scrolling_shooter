a0c09e96b9a4015f69d9b1e21f92ec140e5718fb
rewoop <10rubleqow@gmail.com>
2021-05-11 12:37:03 +0300

    Adds loading bar.

--- assets/scripts/scenes/PreloadScene.js
+++ assets/scripts/scenes/PreloadScene.js
@@ -4,6 +4,12 @@ class PreloadScene extends Phaser.Scene {
 	}
 
 	preload() {
+		this.add.sprite(0, 0, 'bg').setOrigin(0);
+		new LoadingBar(this);
+		this.preloadAssets();
+	}
+
+	preloadAssets() {
 		this.load.image('bullet', 'assets/sprites/bullet.png');
 		this.load.image('fire', 'assets/sprites/fire.png');
 		this.load.atlas('dragon', 'assets/sprites/dragon.png', 'assets/sprites/dragon.json');
